95fec4a490977a3dc3dfc5f5188dbcb10336b1a8
Dustin Glaser <dustin.glaser@posteo.de>
2022-08-15 15:16:38 +0200

    initial implementation

---
+++ src/main/java/h01/Checkers.java
@@ -0,0 +1,297 @@
+package h01;
+
+import fopbot.Direction;
+import fopbot.Robot;
+import fopbot.RobotFamily;
+import fopbot.World;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import static org.tudalgo.algoutils.student.io.PropertyUtils.getIntProperty;
+
+public class Checkers {
+
+    /**
+     * The number of rows in the game board.
+     */
+    public static final int NUMBER_OF_ROWS = getIntProperty("checkers.properties", "NUMBER_OF_ROWS");
+
+    /**
+     * The number of columns in the game board.
+     */
+    public static final int NUMBER_OF_COLUMNS = getIntProperty("checkers.properties", "NUMBER_OF_COLUMNS");
+
+    /**
+     * The minimum initial number of coins for a black stone.
+     */
+    public static final int MIN_NUMBER_OF_COINS = getIntProperty("checkers.properties", "MIN_NUMBER_OF_COINS");
+
+    /**
+     * The maximum initial number of coins for a black stone.
+     */
+    public static final int MAX_NUMBER_OF_COINS = getIntProperty("checkers.properties", "MAX_NUMBER_OF_COINS");
+
+    /**
+     * The current state of the game.
+     * At the start of the game, the state of the game is set to {@link GameState#RUNNING}.
+     * After the game has finished, the state of the game is set to {@link GameState#BLACK_WIN} or {@link GameState#WHITE_WIN}.
+     */
+    private GameState gameState = GameState.RUNNING;
+
+
+    /**
+     * The robot of the white team.
+     */
+    private Robot whiteStone;
+
+    /**
+     * The robots of the black team.
+     */
+    private Robot blackStone0, blackStone1, blackStone2, blackStone3, blackStone4;
+
+    public static void main(String[] args) {
+        Checkers checkers = new Checkers();
+        checkers.initGame();
+        checkers.runGame();
+    }
+
+    /**
+     * Runs the initialization of the game.
+     * The initialization of the game consists of the initialization of the world and all stones.
+     */
+    public void initGame() {
+        // initialize the world
+        World.setSize(NUMBER_OF_COLUMNS, NUMBER_OF_ROWS);
+        // initialize all stones
+        initWhiteStone();
+        initBlackStones();
+    }
+
+    /**
+     * Runs the game. After the game has finished, the winner of the game will be printed to the console.
+     */
+    public void runGame() {
+        World.setVisible(true);
+        while (isRunning()) {
+            doBlackTeamAction();
+            doWhiteTeamAction();
+            updateGameState();
+        }
+        System.out.printf("Final State: %s%n", gameState);
+    }
+
+    /**
+     * Returns {@code true} if the game is running, {@code false} otherwise.
+     *
+     * @return if the game is running
+     */
+    public boolean isRunning() {
+        return gameState == GameState.RUNNING;
+    }
+
+    /**
+     * Runs the initialization of the white stone.
+     */
+    public void initWhiteStone() {
+        whiteStone = createStone(RobotFamily.SQUARE_WHITE);
+    }
+
+    /**
+     * Runs the initialization of all black stones.
+     */
+    public void initBlackStones() {
+        blackStone0 = createStone(RobotFamily.SQUARE_BLACK);
+        blackStone1 = createStone(RobotFamily.SQUARE_BLACK);
+        blackStone2 = createStone(RobotFamily.SQUARE_BLACK);
+        blackStone3 = createStone(RobotFamily.SQUARE_BLACK);
+        blackStone4 = createStone(RobotFamily.SQUARE_BLACK);
+    }
+
+    /**
+     * Runs the action of the black team.
+     */
+    public void doBlackTeamAction() {
+        Robot selectedRobot = null;
+        while (selectedRobot == null || !selectedRobot.hasAnyCoins()) {
+            int selectedNumber = ThreadLocalRandom.current().nextInt(5);
+            selectedRobot = getBlackStone(selectedNumber);
+        }
+        selectedRobot.putCoin();
+        int n;
+        for (n = 0; n < 4; n++) {
+            int nextX = selectedRobot.getX() + nextDeltaX(4 + n - selectedRobot.getDirection().ordinal());
+            int nextY = selectedRobot.getY() + nextDeltaY(4 + n - selectedRobot.getDirection().ordinal());
+            if (!isWhiteStonePosition(nextX, nextY) && isWorldPosition(nextX, nextY)) {
+                break;
+            }
+        }
+        if (n == 0) {
+            selectedRobot.move();
+            selectedRobot.turnLeft();
+            selectedRobot.turnLeft();
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+        } else if (n == 1) {
+            selectedRobot.move();
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+        } else if (n == 2) {
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+        } else if (n == 3) {
+            selectedRobot.turnLeft();
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+            selectedRobot.turnLeft();
+            selectedRobot.move();
+        }
+    }
+
+    private int nextDeltaX(int n) {
+        n = n % 4;
+        return n == 1 || n == 2 ? -1 : 1;
+    }
+
+    private int nextDeltaY(int n) {
+        n = n % 4;
+        return n == 2 || n == 3 ? -1 : 1;
+    }
+
+    /**
+     * Runs the action of the white team.
+     */
+    public void doWhiteTeamAction() {
+        int x = whiteStone.getX();
+        int y = whiteStone.getY();
+        Robot blackStone = null;
+        int xD = 0;
+        int yD = 0;
+        for (int i = 0; i < 4 && blackStone == null; i++) {
+            xD = i == 0 || i == 1 ? 1 : -1;
+            yD = i == 0 || i == 3 ? 1 : -1;
+            int xN = x + xD;
+            int yN = y + yD;
+            while (xN + xD >= 0 && xN + xD < NUMBER_OF_COLUMNS && yN + yD >= 0 && yN + yD < NUMBER_OF_ROWS) {
+                blackStone = getBlackStone(xN, yN);
+                if (blackStone != null) {
+                    if (getBlackStone(xN + xD, yN + yD) != null) {
+                        blackStone = null;
+                    }
+                    break;
+                }
+                xN += xD;
+                yN += yD;
+            }
+        }
+        if (blackStone != null) {
+            whiteStone.setX(blackStone.getX() + xD);
+            whiteStone.setY(blackStone.getY() + yD);
+            blackStone.turnOff();
+        }
+
+    }
+
+    /**
+     * Checks if a team has won the game and, if so, updates the game state to {@link GameState#BLACK_WIN} or {@link GameState#WHITE_WIN}.
+     */
+    public void updateGameState() {
+        boolean whiteCanWin = true, blackCanWin = true;
+        for (int i = 0; i < 5 && (whiteCanWin || blackCanWin); i++) {
+            Robot blackRobot = getBlackStone(i);
+            if (blackRobot.isTurnedOn()) {
+                whiteCanWin = false;
+            }
+            if (blackRobot.isTurnedOn() && blackRobot.hasAnyCoins()) {
+                blackCanWin = false;
+            }
+            if (i == 4) {
+                if (whiteCanWin) {
+                    gameState = GameState.WHITE_WIN;
+                } else if (blackCanWin) {
+                    gameState = GameState.BLACK_WIN;
+                }
+            }
+        }
+
+
+    }
+
+
+    private Robot createStone(RobotFamily family) {
+        int x, y;
+        do {
+            x = getRandom().nextInt(NUMBER_OF_COLUMNS);
+            y = getRandom().nextInt(NUMBER_OF_ROWS);
+        } while ((x + y) % 2 == 0 || (family == RobotFamily.SQUARE_BLACK && x == whiteStone.getX() && y == whiteStone.getY()));
+        Direction direction = Direction.values()[getRandom().nextInt(4)];
+        int coins = getRandom().nextInt(MIN_NUMBER_OF_COINS, MAX_NUMBER_OF_COINS + 1);
+        return new Robot(x, y, direction, coins, family);
+    }
+
+    /**
+     * Returns if the given position is a position within the world.
+     *
+     * @param x the x-coordinate of the position
+     * @param y the y-coordinate of the position
+     * @return if the given position is a position within the world
+     */
+    private boolean isWorldPosition(int x, int y) {
+        return x >= 0 && x < NUMBER_OF_COLUMNS && y >= 0 && y < NUMBER_OF_ROWS;
+    }
+
+    /**
+     * Returns if the white stone is at the given position.
+     *
+     * @param x the x-coordinate of the position
+     * @param y the y-coordinate of the position
+     * @return if the white stone is at the given position
+     */
+    private boolean isWhiteStonePosition(int x, int y) {
+        return whiteStone.getX() == x && whiteStone.getY() == y;
+    }
+
+    /**
+     * Returns the black stone with the given number.
+     *
+     * @param number the number of the black stone
+     * @return the black stone with the given number
+     */
+    private Robot getBlackStone(int number) {
+        return switch (number) {
+            case 0 -> blackStone0;
+            case 1 -> blackStone1;
+            case 2 -> blackStone2;
+            case 3 -> blackStone3;
+            case 4 -> blackStone4;
+            default -> null;
+        };
+    }
+
+    /**
+     * Returns the black stone at the given position or {@code null} if there is no black stone at the given position.
+     *
+     * @param x the x-coordinate of the position
+     * @param y the y-coordinate of the position
+     * @return the black stone at the given position
+     */
+    private Robot getBlackStone(int x, int y) {
+        for (int i = 0; i < 5; i++) {
+            Robot blackStone = getBlackStone(i);
+            if (blackStone.isTurnedOn() && blackStone.isTurnedOn() && blackStone.getX() == x && blackStone.getY() == y) {
+                return blackStone;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns an instance of {@link Random}.
+     *
+     * @return an instance of {@link Random}
+     */
+    private Random getRandom() {
+        return ThreadLocalRandom.current();
+    }
+}
